fa39ad763c839fc85b7aadfe283bd7b808aa97b9
gustavoparro <gustavo.ferreira.parro@gmail.com>
2022-06-24 17:54:00 -0300

    Criando AuthFilter.

---
+++ src/main/java/com/gustavoparro/ecommerce_jsp/filters/AuthFilter.java
@@ -0,0 +1,33 @@
+package com.gustavoparro.ecommerce_jsp.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "AuthFilter", urlPatterns = { "*" })
+public class AuthFilter implements Filter {
+
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpSession httpSession = httpServletRequest.getSession();
+        String user = (String) httpSession.getAttribute("app_user");
+        String servletPath = httpServletRequest.getServletPath();
+
+        if ((user == null || user.isBlank() ) && !servletPath.contains("auth")) {
+            httpServletRequest.getRequestDispatcher("auth/index.jsp").forward(request, response);
+        }
+
+        chain.doFilter(request, response);
+    }
+
+}
