ae77de66c03646a66d76973da1fee98e507e6ecd
gustavoparro <gustavo.ferreira.parro@gmail.com>
2022-06-24 15:33:08 -0300

    Criando a View e a Servlet de Auth.

---
+++ src/main/java/com/gustavoparro/ecommerce_jsp/servlets/AuthServlet.java
@@ -0,0 +1,24 @@
+package com.gustavoparro.ecommerce_jsp.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/auth")
+public class AuthServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("pages/auth/index.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
---
+++ src/main/webapp/pages/auth/index.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+        <!DOCTYPE html>
+<html>
+<head>
+    <title>eCommerce-JSP | Auth</title>
+</head>
+<body>
+    <h1>Login</h1>
+
+    <form action="" method="post">
+        <div>
+            <label for="email">E-mail:</label><br />
+            <input type="email" name="email" id="email">
+        </div>
+
+        <div>
+            <label for="password">Password:</label><br />
+            <input type="password" name="password" id="password">
+        </div>
+
+        <input type="submit" value="Sign in">
+    </form>
+</body>
+</html>
