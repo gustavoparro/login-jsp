3d8af1e872e62fd212a9184b3a9efde5aabf28ff
Gustavo Ferreira Parro <gustavo.ferreira.parra@gmail.com>
2022-06-27 11:35:16 -0300

    Criando diretório para testes de unidade.

---
+++ src/test/java/com/gustavoparro/ecommerce_jsp/InitTest.java
@@ -0,0 +1,12 @@
+package com.gustavoparro.ecommerce_jsp;
+
+import org.junit.jupiter.api.Test;
+
+public class InitTest {
+
+    @Test
+    public void hello() {
+        System.out.println("Hello, World!");
+    }
+
+}
